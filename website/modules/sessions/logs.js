0c14cc6d444d77f936c074e1ba5d738814d05db0
Mattox0 <matbegaaar@gmail.com>
2023-05-16 11:06:31 +0200

    Dashboard logs front finish + back loading

--- website/modules/sessions/logs.js
+++ website/modules/sessions/logs.js
@@ -22,19 +22,63 @@ async function isValidLogs(body, guild) {
 async function updateLogs(body, id) {
 	const con = index.getDB();
 	console.log(body);
-	await con.promise().query(`INSERT INTO logs (guild_id, logs_id, channelCreate, channelDelete, channelUpdate) 
+	await con.promise().query(`INSERT INTO logs (guild_id, logs_id, channelCreate, channelDelete, channelUpdate, messageDelete, messageUpdate, roleCreate, roleDelete, roleUpdate, emojiCreate, emojiDelete, emojiUpdate, voiceStateUpdate, guildMemberAdd, guildMemberRemove, guildBanAdd, guildBanRemove, inviteCreate, inviteDelete, stickerCreate, stickerDelete, threadCreate, threadDelete, pseudo, boost, guildScheduledEventCreate) 
 	VALUES (
 		'${id}', 
 		'${body.logs_channel_id}', 
 		${body.logs_channel_update ? 1 : 0}, 
 		${body.logs_channel_update ? 1 : 0}, 
-		${body.logs_channel_update ? 1 : 0}
+		${body.logs_channel_update ? 1 : 0},
+		${body.logs_message ? 1 : 0},
+		${body.logs_message ? 1 : 0},
+		${body.logs_role ? 1 : 0},
+		${body.logs_role ? 1 : 0},
+		${body.logs_role ? 1 : 0},
+		${body.logs_emoji ? 1 : 0},
+		${body.logs_emoji ? 1 : 0},
+		${body.logs_emoji ? 1 : 0},
+		${body.logs_vocal ? 1 : 0},
+		${body.logs_member ? 1 : 0},
+		${body.logs_member ? 1 : 0},
+		${body.logs_ban ? 1 : 0},
+		${body.logs_ban ? 1 : 0},
+		${body.logs_invite ? 1 : 0},
+		${body.logs_invite ? 1 : 0},
+		${body.logs_sticker ? 1 : 0},
+		${body.logs_sticker ? 1 : 0},
+		${body.logs_thread ? 1 : 0},
+		${body.logs_thread ? 1 : 0},
+		${body.logs_pseudo ? 1 : 0},
+		${body.logs_boost ? 1 : 0},
+		${body.logs_event ? 1 : 0}
 		) 
 	ON DUPLICATE KEY UPDATE 
 		logs_id = '${body.logs_channel_id}',
 		channelCreate = ${body.logs_channel_update ? 1 : 0},
 		channelDelete = ${body.logs_channel_update ? 1 : 0},
-		channelUpdate = ${body.logs_channel_update ? 1 : 0}
+		channelUpdate = ${body.logs_channel_update ? 1 : 0},
+		messageDelete = ${body.logs_message ? 1 : 0},
+		messageUpdate = ${body.logs_message ? 1 : 0},
+		roleCreate = ${body.logs_role ? 1 : 0},
+		roleDelete = ${body.logs_role ? 1 : 0},
+		roleUpdate = ${body.logs_role ? 1 : 0},
+		emojiCreate = ${body.logs_emoji ? 1 : 0},
+		emojiDelete = ${body.logs_emoji ? 1 : 0},
+		emojiUpdate = ${body.logs_emoji ? 1 : 0},
+		voiceStateUpdate = ${body.logs_vocal ? 1 : 0},
+		guildMemberAdd = ${body.logs_member ? 1 : 0},
+		guildMemberRemove = ${body.logs_member ? 1 : 0},
+		guildBanAdd = ${body.logs_ban ? 1 : 0},
+		guildBanRemove = ${body.logs_ban ? 1 : 0},
+		inviteCreate = ${body.logs_invite ? 1 : 0},
+		inviteDelete = ${body.logs_invite ? 1 : 0},
+		stickerCreate = ${body.logs_sticker ? 1 : 0},
+		stickerDelete = ${body.logs_sticker ? 1 : 0},
+		threadCreate = ${body.logs_thread ? 1 : 0},
+		threadDelete = ${body.logs_thread ? 1 : 0},
+		pseudo = ${body.logs_pseudo ? 1 : 0},
+		boost = ${body.logs_boost ? 1 : 0},
+		guildScheduledEventCreate = ${body.logs_event ? 1 : 0}
 		`,)
 	await con.promise().query(`UPDATE servers SET logs_id = '${body.logs_channel_id}' WHERE guild_id = '${id}'`)
 }
