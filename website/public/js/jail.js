7150eca54f13e5f039846a8b15422e3da168ff4e
Mattox0 <matbegaaar@gmail.com>
2023-06-05 19:25:34 +0200

    Website -> Dashboard -> Prison

---
+++ website/public/js/jail.js
@@ -0,0 +1,13 @@
+const create_jail = document.querySelector('#create-jail');
+const delete_jail = document.querySelector('#delete-jail');
+
+create_jail.addEventListener('click', (e) => {
+	// creer un channel
+	// creer un role
+	// setup les permissions de tous les channels
+	
+});
+
+delete_jail.addEventListener('click', (e) => {
+
+});
